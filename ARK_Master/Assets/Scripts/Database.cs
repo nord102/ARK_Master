bee3ce83c1d98a637f7280554ae720b4c452abb3
J-Bones <Josh.M.Broomfield@gmail.com>
2016-03-09 02:30:18 -0500

    Added and changed dialogue box from old project and changed the db
    updated and added

---
+++ ARK_Master/Assets/Scripts/Database.cs
@@ -0,0 +1,318 @@
+﻿using UnityEngine;
+using System.Collections;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using System.Collections.Generic;
+
+//http://answers.unity3d.com/questions/743400/database-sqlite-setup-for-unity.html
+//64 bit dll http://blog.synopse.info/post/2013/03/23/Latest-version-of-sqlite3.dll-for-Windows-64-bit
+public class Database
+{
+    private string conn; //Path to database.
+    private string lootColumnsNoId;
+    private string lootColumns;
+
+    public Database(string applicationDataPath)
+    {
+        conn = "URI=file:" + applicationDataPath + "/Databases/DB.db"; //Path to database.
+        lootColumnsNoId = " RewardName, RewardImagePath, RewardTimer, HPChange, EnergyChange, ShieldChange, ShipResourcesFound, SkillFound, CharacterUnlocked, LootTableValue ";
+        lootColumns = " Id," + lootColumnsNoId;
+
+        /* Usage example
+        Database db = new Database(Application.dataPath);
+
+        Loot hi = db.Find(1);
+        db.Delete(3);
+        List<Loot> list = db.Search("h");
+        foreach (Loot z in list)
+        {
+            db.Insert(z);
+        }
+
+
+        Loot a = new Loot();
+        db.Insert(a);
+
+        List<Loot> random = db.GetRandomRows(5, 4);
+        Loot r = db.GetRandomRow(5);*/
+    }
+
+    public Rewards GetRandomRow(int lootTableValue, int eventType = 0)
+    {
+        Rewards ret = null;
+
+        List<Rewards> lootList = GetRandomRows(lootTableValue, 1, eventType);
+        if (lootList.Count != 0)
+        {
+            ret = lootList[0];
+        }
+
+        return ret;
+    }
+
+    public List<Rewards> GetRandomRows(int lootTableValue, int numRows = 1, int eventType = 0)
+    {
+        bool includeEventType = eventType == 0 ? false : true;
+        string extraSearch = "";
+
+        if(includeEventType)
+        {
+            extraSearch = " AND EventType = @EventType ";
+        }
+
+        string sqlQuery = "SELECT " + lootColumns + " FROM Reward WHERE LootTableValue = @LootTableValue " + extraSearch + " ORDER BY RANDOM() LIMIT @NumRows;";
+
+        List<DBParameter> parameters = new List<DBParameter>();
+        parameters.Add(new DBParameter("@LootTableValue", lootTableValue));
+        parameters.Add(new DBParameter("@NumRows", numRows));
+        if(includeEventType)
+        {
+            parameters.Add(new DBParameter("@EventType", eventType));
+        }
+
+        DBResults results = ExecuteReader(sqlQuery, parameters);
+
+        List<Rewards> loot = GetList(results);
+
+        return loot;
+    }
+
+    /// <summary
+    /// Searches with LIKE %RewardName%
+    /// </summary>
+    /// <param name="RewardName"></param>
+    /// <returns></returns>
+    public List<Rewards> Search(string RewardName)
+    {
+        string sqlQuery = "SELECT " + lootColumns + " FROM Reward WHERE RewardName LIKE @RewardName";
+        List<DBParameter> parameters = new List<DBParameter>();
+        parameters.Add(new DBParameter("@RewardName", "%" + RewardName + "%"));
+
+        DBResults results = ExecuteReader(sqlQuery, parameters);
+
+        return GetList(results);
+    }
+
+    public void Insert(Rewards loot)
+    {
+        string sqlQuery = @"
+            INSERT INTO Reward(" + lootColumnsNoId + @") VALUES 
+            (@Id, @RewardName, @RewardImagePath, @RewardTimer, @HPChange, @EnergyChange, @ShieldChange, @ShipResourcesFound, @SkillFound, @CharacterUnlocked, @LootTableValue)";
+
+        List<DBParameter> parameters = new List<DBParameter>();
+        parameters.Add(new DBParameter("@Id", loot.Id));
+        parameters.Add(new DBParameter("@RewardName", loot.RewardName));
+        parameters.Add(new DBParameter("@RewardImagePath", loot.RewardImagePath));
+        parameters.Add(new DBParameter("@RewardTimer", loot.RewardTimer));
+        parameters.Add(new DBParameter("@HPChange", loot.HPChange));
+        parameters.Add(new DBParameter("@EnergyChange", loot.EnergyChange));
+        parameters.Add(new DBParameter("@ShieldChange", loot.ShieldChange));
+        parameters.Add(new DBParameter("@ShipResourcesFound", loot.ShipResourcesFound));
+        parameters.Add(new DBParameter("@SkillFound", loot.SkillFound));
+        parameters.Add(new DBParameter("@CharacterUnlocked", loot.CharacterUnlocked));
+        parameters.Add(new DBParameter("@LootTableValue", loot.LootTableValue));
+
+        ExecuteNonQuery(sqlQuery, parameters);
+    }
+
+    public void Delete(int Id)
+    {
+        string sqlQuery = "DELETE FROM Reward WHERE Id = @Id";
+
+        List<DBParameter> parameters = new List<DBParameter>();
+        parameters.Add(new DBParameter("@Id", Id));
+
+        ExecuteNonQuery(sqlQuery, parameters);
+    }
+
+    public Rewards Find(int Id)
+    {
+        Rewards found = null;
+        string sqlQuery = "SELECT " + lootColumns + " FROM Reward WHERE Id = @Id ";
+
+        DBParameter pId = new DBParameter("@Id", Id);
+        List<DBParameter> parameters = new List<DBParameter>();
+        parameters.Add(pId);
+
+        DBResults results = ExecuteReader(sqlQuery, parameters);
+
+        List<Rewards> list = GetList(results);
+        if(list.Count != 0)
+        {
+            found = list[0];
+        }
+
+        results.Close();
+
+        return found;
+    }
+
+    private DBResults ExecuteReader(string commandText, List<DBParameter> parameters)
+    {
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        dbcmd.CommandText = commandText;
+
+        AddParameters(dbcmd, parameters);
+        
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        DBResults ret = new DBResults(dbconn, dbcmd, reader);
+        return ret;
+    }
+
+    private int ExecuteNonQuery(string commandText, List<DBParameter> parameters)
+    {
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        dbcmd.CommandText = commandText;
+
+        AddParameters(dbcmd, parameters);
+
+        return dbcmd.ExecuteNonQuery();
+    }
+
+    private void AddParameters(IDbCommand dbcmd, List<DBParameter> parameters)
+    {
+        IDbDataParameter parameter = null;
+        foreach (DBParameter param in parameters)
+        {
+            parameter = dbcmd.CreateParameter();
+            parameter.ParameterName = param.Name;
+            parameter.Value = param.Value;
+            dbcmd.Parameters.Add(parameter);
+        }
+    }
+
+    private List<Rewards> GetList(DBResults results)
+    {
+        Rewards found = null;
+        List<Rewards> ret = new List<Rewards>();
+        while (results.reader.Read())
+        {
+            int Id = results.reader.GetInt32(0);
+            string RewardName = null;
+            if (!results.reader.IsDBNull(1))
+            {
+                RewardName = results.reader.GetString(1);
+            }
+            string RewardImagePath = null;
+            if (!results.reader.IsDBNull(2))
+            {
+                RewardImagePath = results.reader.GetString(2);
+            }
+            int RewardTimer = results.reader.GetInt32(3);
+            int HPChange = results.reader.GetInt32(4);
+            int EnergyChange = results.reader.GetInt32(5);
+            int ShieldChange = results.reader.GetInt32(6);
+            int ShipResourcesFound = results.reader.GetInt32(7);
+            int SkillFound = results.reader.GetInt32(8);
+            int CharacterUnlocked = results.reader.GetInt32(9);
+            int LootTableValue = results.reader.GetInt32(10);
+            int EventType = results.reader.GetInt32(11);
+
+            found = new Rewards(Id, RewardName, RewardImagePath, RewardTimer, HPChange, EnergyChange, ShieldChange, ShipResourcesFound, SkillFound, CharacterUnlocked, LootTableValue, EventType);
+
+            ret.Add(found);
+            Debug.Log("id= " + Id + "  rewardName =" + RewardName);
+        }
+        results.Close();
+        return ret;
+    }
+}
+
+//Easier that writing all this out again when using multiple queries
+public class DBResults
+{
+    public IDbConnection dbconn;
+    public IDbCommand dbcmd;
+    public IDataReader reader;
+    private bool closed;
+
+    public DBResults(IDbConnection dbconn, IDbCommand dbcmd, IDataReader reader)
+    {
+        this.dbconn = dbconn;
+        this.dbcmd = dbcmd;
+        this.reader = reader;
+
+        closed = false;
+    }
+
+    public void Close()
+    {
+        try // Will fail if its a NonQuery. Not a big deal just need to make sure cleanup code after this runs
+        {
+            reader.Close();
+            reader = null;
+        }
+        catch(Exception) { }
+
+        try
+        {
+            dbcmd.Dispose();
+            dbcmd = null;
+            dbconn.Close();
+            dbconn = null;
+        }
+        catch(Exception) { }
+
+        closed = true;
+    }
+
+    ~DBResults()
+    {
+        if (!closed)
+        {
+            Close();
+        }
+    }
+}
+
+/// <summary>
+/// Couldn't find a way to init an IDbCommand parameter without initializing through IDbCommand.CreateParameter
+/// </summary>
+public class DBParameter
+{
+    public string Name;
+    public object Value;
+
+    public DBParameter(string Name, object Value)
+    {
+        this.Name = Name;
+        this.Value = Value;
+    }
+}
+
+/*public class Loot
+{
+    public int Id { get; set; }
+    public string RewardName { get; set; }
+    public int RewardType { get; set; }
+    public int RewardValue { get; set; }
+    public string StatAffected { get; set; }
+    public int RewardTimer { get; set; }
+    public string RewardImage { get; set; }
+    public int LootTableValue { get; set; }
+
+    public Loot()
+    {
+
+    }
+
+    public Loot(int id, string rewardName, int rewardType, int rewardValue, string statAffected, int rewardTimer, string rewardImage, int lootTableValue)
+    {
+        Id = id;
+        RewardName = rewardName;
+        RewardType = rewardType;
+        RewardValue = rewardValue;
+        StatAffected = statAffected;
+        RewardTimer = rewardTimer;
+        RewardImage = rewardImage;
+        LootTableValue = lootTableValue;
+    }
+}*/
---
+++ ARK_Master/Assets/Scripts/Events/MyCanvas.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class MyCanvas : MonoBehaviour
+{
+    private Events MyEvent { get; set; }
+	//public GameObject ChoicePrefab;
+    //public GameObject Canvas;
+    public Text txtTitle;
+    public Text txtDescription;
+    //public GameObject Choices;
+    public GameObject MyImage;
+
+	//private List<GameObject> txtChoices = new List<GameObject>();
+
+	//for button click event
+	//http://answers.unity3d.com/questions/791573/46-ui-how-to-apply-onclick-handler-for-button-gene.html
+    public MyCanvas()
+    {
+
+    }
+
+    public void StartEvent(Events newEvent)
+    {
+        MyEvent = newEvent;
+        StartEvent();
+    }
+
+    private void StartEvent()
+    {
+        //MyImage.GetComponent<Image>().sprite = MyEvent.eventImage.sprite;
+        txtTitle.text = MyEvent.eventName;
+        txtDescription.text = MyEvent.eventText;
+
+        /*Used when this had different options to choose from
+		GameObject newOption = null;
+        Text text = null;
+        EventTrigger eventTrigger = null;
+        int index = 0;
+
+		int displacementY = 5;
+		 
+        foreach(EventChoice ec in MyEvent.eventChoices)
+        {
+            ++index;
+			newOption = Instantiate(ChoicePrefab) as GameObject;
+
+			newOption.transform.SetParent(Choices.transform);
+			newOption.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+			newOption.transform.position = new Vector3(-10, 0 + (displacementY * (index - 1)), Choices.transform.position.z);
+
+            //eventTrigger = newOption.GetComponent<EventTrigger>();
+            //EventTrigger.Entry clickCallback = new EventTrigger.Entry();
+            text = newOption.GetComponent<Text>();
+
+
+            //clickCallback.eventID = EventTriggerType.PointerClick;
+            //clickCallback.callback.AddListener((eventdata) => ChoiceMade(index));
+            text.text = index.ToString() + ") " + ec.choiceText;
+
+			//newOption.GetComponent<RectTransform>().sizeDelta = new Vector2 (455, 18);
+
+
+			Button b = newOption.GetComponent<Button>();
+			b.onClick.AddListener(() => ChoiceMade (index - 1));
+
+
+			txtChoices.Add(newOption);
+        }*/
+
+		//gameObject.SetActive (true);
+    }
+    
+  //  public void ChoiceMade(int choice)
+  //  {
+		//for(int i = 0; i < txtChoices.Count; ++i)
+		//{
+		//	txtChoices[i].transform.SetParent(null);
+		//	Destroy (txtChoices[i]);
+		//}
+		//txtChoices.Clear();
+
+		//GameObject newOption = Instantiate(ChoicePrefab) as GameObject;
+		//newOption.transform.SetParent(Choices.transform);
+		//newOption.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+		//newOption.transform.position = new Vector3(-10, 0, Choices.transform.position.z);
+
+		////MyEvent.eventChoices [choice].ResolveEvent ();
+		//txtDescription.text = MyEvent.eventChoices [choice].FinalText;
+
+		//Text text = newOption.GetComponent<Text>();
+		//text.text = "Continue";
+
+		//Button b = newOption.GetComponent<Button>();
+		//b.onClick.AddListener(() => CloseCanvas());
+
+		////gameObject.SetActive (false);
+  //  }
+
+	public void CloseCanvas()
+	{
+		Destroy (gameObject);
+	}
+}
