8b976c77fe887dca5c65b3da83412ed1092268ec
nadnadsetset <nadnadsetset@gmail.com>
2016-04-03 11:19:47 -0400

    recovered Extingusher file

---
+++ ARK_Master/Assets/Scripts/Other/ExtinguishSpray.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtinguishSpray : MonoBehaviour {
+
+	public Animator animator;
+	Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (this.direction);
+		if (this.animator.isActiveAndEnabled== false) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	public void Trajectory(Vector3 v)
+	{
+
+		this.direction = v;
+	}
+}
